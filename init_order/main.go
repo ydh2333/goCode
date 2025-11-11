8740a6fe61aab60f6b3a550f082db8dda808a23b
yangduheng <1064723285@qq.com>
2025-11-11 14:25:14 +0800

    更改1

--- init_order/main.go
+++ init_order/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 
-    _ "github.com/ydh2333/goCode/init_order/pkg1"
+	_ "github.com/ydh2333/goCode/init_order/pkg1"
 )
 
 const mainName string = "main"
@@ -11,14 +11,15 @@ const mainName string = "main"
 var mainVar string = getMainVar()
 
 func init() {
-    fmt.Println("main init method invoked")
+	fmt.Println("main init method invoked")
 }
 
 func main() {
-    fmt.Println("main method invoked!")
+	fmt.Println("main method invoked!")
+	fmt.Println("hello world!!!")
 }
 
 func getMainVar() string {
-    fmt.Println("main.getMainVar method invoked!")
-    return mainName
+	fmt.Println("main.getMainVar method invoked!")
+	return mainName
 }
